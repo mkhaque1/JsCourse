00f85c881b01d15a6bbcafcee3e11c498b1497ec
mkhaque1 <mkhaque29@gmail.com>
2026-05-25 14:32:45 +0200

    update

---
+++ arithmeticOperators.js
@@ -0,0 +1,30 @@
+// jog kora
+
+let a = 10;
+let b = 20;
+
+let c = 'hello';
+let d = 'world';
+
+console.log(a + b);
+console.log(c + d);
+
+// minus
+
+console.log(b - a);
+
+// multiply
+
+console.log(a * b);
+
+// devide
+
+console.log(a / b);
+
+// jor , bejor check korte
+
+console.log(10 % 3); // 1
+
+// power
+
+console.log(2 ** 10);
---
+++ numberMethods.js
@@ -0,0 +1,30 @@
+let purnosongkha = 42;
+
+let dashomik = 3.14;
+
+let negetive = -10;
+
+// special value
+
+let khali = NaN;
+let inft = Infinity;
+
+// parseInt
+
+console.log(parseInt('25px'));
+
+// Boolean
+
+let login = true;
+let error = false;
+
+// comparison
+let boro = 5 > 3;
+
+console.log(boro);
+
+//Falsy values
+
+// fasle, 0, '', null, undefined, NaN
+
+// all are truthy value
