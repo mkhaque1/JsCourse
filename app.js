16cc1f2981227e80795647544f6a453d09e7b389
mkhaque1 <mkhaque29@gmail.com>
2026-05-25 12:56:35 +0200

    data type, string

--- app.js
+++ app.js
@@ -4,8 +4,32 @@
 
 // var, let , const
 
-let name = 'sakib'; // value poriborton
+let naam = 'sakib'; // value poriborton
 const boys = 25; // value oporiborton
 
-console.log(name);
+console.log(naam);
 console.log(boys);
+
+const name = 'sakib'; // string data type
+let age = 35; // number data stype
+
+console.log(name);
+console.log(age);
+
+// Boolean
+
+let isLogin = false; // boolean
+
+console.log(isLogin);
+
+// Null
+
+let data = null;
+
+console.log(data);
+
+// undefined
+
+let x;
+
+console.log(x);
