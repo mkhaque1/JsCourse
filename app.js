3a1164eda8e01a13ce3568a7b5b5baac12d08efa
mkhaque1 <mkhaque29@gmail.com>
2026-05-25 12:35:32 +0200

    js course - 001

---
+++ app.js
@@ -0,0 +1,11 @@
+// JavaScript varriables and data types
+
+// var, let, const
+
+// var, let , const
+
+let name = 'sakib'; // value poriborton
+const boys = 25; // value oporiborton
+
+console.log(name);
+console.log(boys);
