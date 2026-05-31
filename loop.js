0a84ef1fda3537ae1ec50ce2307c7f103cfa234e
mkhaque1 <mkhaque29@gmail.com>
2026-05-31 16:48:39 +0200

    js loop

---
+++ loop.js
@@ -0,0 +1,53 @@
+console.log('1');
+console.log('2');
+console.log('3');
+console.log('4');
+console.log('5');
+
+// for LOOP STRUCTURE : for (initialization; condition; update) {}
+// initialization let = 1 ;
+// condition i <= 5 ;
+// update i++ ;
+
+// Real Projects
+
+// ✅ Product List
+
+// ✅ Student List
+
+// ✅ Comments
+
+// ✅ Notifications
+
+// ✅ Chat Messages
+
+// ✅ API Data
+
+// ✅ Orders
+
+// ✅ Users
+
+// example
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+// student roll :
+
+for (let roll = 1; roll <= 5; roll++) {
+  console.log('students roll:', roll);
+}
+
+// modern js loop :
+
+const products = [
+  'notification 1',
+  'notification 2',
+  'notification 3',
+  'notification 4',
+];
+
+for (const product of products) {
+  console.log(product);
+}
