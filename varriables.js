1e0b7cd3e416a2dda52c01cd8159a6ab15d0f24d
mkhaque1 <mkhaque29@gmail.com>
2026-06-16 22:07:09 +0200

    varriable and string data type

---
+++ varriables.js
@@ -0,0 +1,20 @@
+// varriables
+
+// old js
+var name = 'old';
+
+// modern js
+const firstName = 'mk';
+let lastName = 'haque';
+
+console.log(`my name is ${firstName} ${lastName} `); // debug purpose
+
+// string data type
+
+// 'data', " data "
+
+let naaam = 'sakib'; // value poriborton
+const boys = 25; // value oporiborton
+
+console.log(naaam);
+console.log(boys);
